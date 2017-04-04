1a10a7b28c6d891de18bfc1b78070a78021ac1cc
nikhilj <nikhilj@mindfiresolutions.com>
2017-04-04 13:13:42 +0530

    Added constants' file and barel file

---
+++ ccl-ng2/src/app/shared/constant/storage.key.constant.ts
@@ -0,0 +1,14 @@
+/**
+ * storage-key.constant.ts
+ * 
+ * This file contains strings constants that ar application wides. 
+ */
+
+/**
+ *  The StorageKeys object
+ */
+export const StorageKeys = {
+    LOGIN_LAYOUT_BG: 'loginBg',
+    AUTH_TOKEN: 'token',
+    LOGGED_IN_USER: 'user'
+};
